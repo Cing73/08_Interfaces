b9ca1d4d9f663ddc588005e0fc397fe320493aef
Cing73 <craig@vancleaf>
2022-07-27 11:46:31 +0200

    zusammen

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -8,16 +8,23 @@ public class App {
         HoneyBee queen = new Queen();
         HoneyBee drone = new Drone();
 
+        Bird bird = new Bird();
+
+        output("----- Bees -----");
         output(worker.doYourJob());
         output(queen.doYourJob());
         output(drone.doYourJob());
 
+        output("---- Bird(s) ------");
+        output(bird.hasFeathers());
 
         output("----------------");
 
+        output("---- Birds & Bees ------");
         output(worker.fly());
         output(queen.fly());
         output(drone.fly());
+        output(bird.fly());
 
     }
 
